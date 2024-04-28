68f3c0e790a89be8408fd3ad8f3f6c331379e871
Entity378 <150670165+Entity378@users.noreply.github.com>
2024-04-28 02:04:43 +0200

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,27 +1,21 @@
-﻿using BepInEx;
+using BepInEx;
 using HarmonyLib;
 
 namespace BetterShotgun
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    [BepInProcess("Lethal Company.exe")]
     public class Plugin : BaseUnityPlugin
     {
-        private Harmony h;
+        readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
             ShotgunConfig.LoadConfig(Config);
             Logger.LogInfo("Config loaded");
-
-            h = Harmony.CreateAndPatchAll(typeof(ShotgunPatch));
-            h.PatchAll(typeof(ShotgunConfig));
-        }
-
-        private void OnDestroy()
-        {
-            h.UnpatchSelf();
+            harmony.PatchAll(typeof(ShotgunPatch));
+            harmony.PatchAll(typeof(ShotgunConfig));
         }
     }
 }
